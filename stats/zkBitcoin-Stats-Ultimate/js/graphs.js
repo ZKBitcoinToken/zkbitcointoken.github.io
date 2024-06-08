0fb80f3f1fa12ba4e90ee1a1b2b36770edebd201
zkBitcoin Token <128942687+ZKBitcoinToken@users.noreply.github.com>
2024-06-08 12:25:43 -0500

    Update graphs.js

--- stats/zkBitcoin-Stats-Ultimate/js/graphs.js
+++ stats/zkBitcoin-Stats-Ultimate/js/graphs.js
@@ -1258,7 +1258,7 @@ log("start_eth_block", start_eth_block)
       + tokens_price_values2.expected_state_length
       + last_diff_start_blocks.expected_state_length;
       + tokens_price_values4.expected_state_length;
-    show_progress((70 * (numerator/denominator)).toFixed(0)
+    show_progress(50 * (numerator/denominator)).toFixed(0)
                   + '% ['
                   + numerator.toFixed(0)
                   + ' / '
@@ -1277,7 +1277,7 @@ log("start_eth_block", start_eth_block)
       + tokens_minted_values.expected_state_length
       + era_values.expected_state_length
       + last_diff_start_blocks.expected_state_length;
-    show_progress((70 * (numerator/denominator)).toFixed(0)
+    show_progress((100 * (numerator/denominator)).toFixed(0)
                   + '% ['
                   + numerator.toFixed(0)
                   + ' / '
