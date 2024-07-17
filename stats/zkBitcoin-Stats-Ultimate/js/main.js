15d46aa69880e1242d98d9c0a902ed7be36fce04
zkBitcoin Token <128942687+ZKBitcoinToken@users.noreply.github.com>
2024-07-17 13:37:27 -0500

    Update main.js

--- stats/zkBitcoin-Stats-Ultimate/js/main.js
+++ stats/zkBitcoin-Stats-Ultimate/js/main.js
@@ -1017,10 +1017,18 @@ var total_TOTAL_mint_count_HASH = 0;
 		  
         if (miner_block_count[mintData[2]] === undefined) {
           miner_block_count[mintData[2]] =  mintData[3]/50;
-          miner_block_count2[mintData[2]] =  1;
+	  if(miner_block_count2[mintData[2]]  === undefined &&  mintData[3] ! = 0) {
+		  miner_block_count2[mintData[2]] =  1;
+	  }else if(  mintData[3] ! = 0 ){
+		miner_block_count2[mintData[2]] +=  1;
+	  }
         } else {
           miner_block_count[mintData[2]]+=  mintData[3]/50;
-          miner_block_count2[mintData[2]]+=  1;
+	  if(miner_block_count2[mintData[2]]  === undefined &&  mintData[3] ! = 0) {
+		  miner_block_count2[mintData[2]] =  1;
+	  }else if(  mintData[3] ! = 0 ){
+		miner_block_count2[mintData[2]] +=  1;
+	  }
         }
 	if(mintData[3] !=0){
 	    total_tx_count+=1;
